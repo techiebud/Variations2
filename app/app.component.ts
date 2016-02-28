15f8e9f10aab745f1e5f6844ddc73ce1e4bc7622
Johnny Sims <techiebud@gmail.com>
2016-02-28 18:55:33 -0500

    Began rearchitecting the files using Angular2 which will help reduce duplicate code.

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "angular2/core";
+
+@Component({
+    selector: "var-main",
+    templateUrl: "app/app.component.html",
+})
+
+export class AppComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
