c79d29bbf68a1a12e1ecd35f114b7a12a8136a83
lilkube <lilkube@gmail.com>
2016-05-19 20:27:31 -0400

    units for sale

---
+++ app/forsale.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from "angular2/core"
+
+@Component({
+       templateUrl: "app/forsale.component.html"
+})
+
+export class ForsaleComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
