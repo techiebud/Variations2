aa7097d181244c7d82150e03dc80b81a9e4a3d76
Johnny Sims <techiebud@gmail.com>
2016-05-31 15:50:20 -0400

    add firebase publishing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    entry: ["./app/main.js", "./js/bootstrap.js"],
+    entry: ["./js/bootstrap.js", 
+    "./node_modules/core-js/client/shim.min.js", 
+    "./node_modules/zone.js/dist/zone.js",
+    "./node_modules/reflect-metadata/Reflect.js","./app/main.js"],
     output:  {
-        filename: "bundle.js"        
+        filename: "public/app.bundle.js"        
     }    
         
 }
