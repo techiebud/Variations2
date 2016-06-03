180f2f74420419e8cc395242301d5cc4ae59c572
Johnny Sims <techiebud@gmail.com>
2016-06-03 11:48:09 -0400

    Fixed Countdown Timer issues

--- app/fees.component.ts
+++ app/fees.component.ts
@@ -1,13 +1,40 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, OnDestroy} from "@angular/core";
 import {AppSettings}  from "./app.component";
 
+declare var $: any;
+declare var tc_widget_loader: any;
+const LOAN_PAYOFF_HEADER: string = "Loan Payoff Countdown";
+
 @Component({
-      templateUrl: "app/fees.component.html"
+    templateUrl: "app/fees.component.html"
 })
 
-export class FeesComponent implements OnInit {
+export class FeesComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    constructor() {
 
-    ngOnInit() { }
+    }
+
+    ngOnInit() {
+        this.countDownTimerInit();
+    }
+
+    ngOnDestroy() {
+        var element = document.getElementById("countdown-script");
+        element.parentElement.removeChild(element);
+
+    }
+
+    countDownTimerInit() {
+        var s = document.createElement('script');
+        s.id = "countdown-script";
+        s.src = "http://www.tickcounter.com/loader.js";
+        s.async = true;
+        s.onload = () => {
+            tc_widget_loader('tc_div_51480', 'Countdown', 650, ["1483264800000", "america-new_york", "dhms", "FFFFFF3B5998000000FF0000", "650", "C0C0C01", LOAN_PAYOFF_HEADER]);
+        };
+       // s.onreadystatechange = s.onload; 
+        var head = document.getElementsByTagName('head')[0]; 
+        head.appendChild(s);
+    }
 }
