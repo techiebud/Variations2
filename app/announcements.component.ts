7e927c4de48cecd44646e23e1061a93b020cfafc
Johnny Sims <techiebud@gmail.com>
2016-06-05 20:17:40 -0400

    twitter testing fixes

--- app/announcements.component.ts
+++ app/announcements.component.ts
@@ -88,17 +88,5 @@ export class AnnouncementsComponent implements OnInit {
 
     }
 
-
-    tweetCode(d, s, id): void {
-        var js,
-            fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-
-        if (!d.getElementById(id)) {
-            js = d.createElement(s); js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-    }
-
+  
 }
