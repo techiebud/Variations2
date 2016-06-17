455365052df3e882791938909c6a36dd4b47dc91
Johnny Sims <techiebud@gmail.com>
2016-06-17 11:15:11 -0400

    added new firebase app.  For amentites, pointed Clubhouse pictures to old web site for now as Firebase will not serve them up correctly.

--- app/shared/firebase.service.ts
+++ app/shared/firebase.service.ts
@@ -7,10 +7,10 @@ export class FirebaseService {
 
     constructor() { 
         var config = {
-           apiKey: "AIzaSyBsMzDuo5sUtsmKxkRM4p7sKWnmAhSVTDo",
-            authDomain: "variatons2.firebaseapp.com",
-            databaseURL: "https://variatons2.firebaseio.com",
-            storageBucket: "variatons2.appspot.com",
+          apiKey: "AIzaSyDOEDQzTcjBQ5Z5i4EAyGAJ1bvYdVcufZ0",
+          authDomain: "project-5333406827865431386.firebaseapp.com",
+          databaseURL: "https://project-5333406827865431386.firebaseio.com",
+          storageBucket: "",
         };
         firebase.initializeApp(config);
 
