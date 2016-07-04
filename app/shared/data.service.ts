82c3e5feca226b8871a784e20f5bb987735de552
Johnny Sims <techiebud@gmail.com>
2016-07-04 08:23:05 -0400

    Added checks to make sure the users browser can support localStorage (mainly a problem with Microsoft Internet Explorer (older versions) and Microsoft Edge private browsing.

--- app/shared/data.service.ts
+++ app/shared/data.service.ts
@@ -8,35 +8,54 @@ declare var md5: any;
 
 @Injectable()
 export class DataService {
-
+    supportsLocalStorage: boolean = false;
     private _userProfileUpdated = new EventEmitter<any>();
+    private _allUnitsLoaded = new EventEmitter<any>();
 
-    constructor(private _firebaseService: FirebaseService) { }
+    constructor(private _firebaseService: FirebaseService) {
+        this.supportsLocalStorage = this.hasLocalStorage();
+
+    }
+
+    private hasLocalStorage(): boolean {
+
+        var uid = "testingStorage";
+        var result = false;
+        try {
+            debugger;
+            localStorage.setItem("uid", uid);
+            result = localStorage.getItem("uid") == uid;
+            localStorage.removeItem(uid);
+            return result;
+            //  return (result && <any>localStorage);
+        } catch (exception) { }
+
+    }
 
     getAnnouncements(): void {
         let fbTable = "Announcements";
-        let databaseRef = firebase.database().ref(fbTable).orderByKey();   
+        let databaseRef = firebase.database().ref(fbTable).orderByKey();
         databaseRef.once('value',
-            (snapshot) => {              
-                let returnedData = snapshot.val();             
+            (snapshot) => {
+                let returnedData = snapshot.val();
                 localStorage.setItem(fbTable, JSON.stringify(returnedData));
             },
-            (error) => {              
+            (error) => {
                 localStorage.setItem(fbTable, "error");
                 console.error(error);
                 toastr.error(error.message);
             });
-    }    
+    }
 
-    getBoardMembers(): void {       
+    getBoardMembers(): void {
         let fbTable = "BoardMembers";
         let databaseRef = firebase.database().ref(fbTable);
         databaseRef.once('value',
-            (snapshot) => {              
-                let returnedData = snapshot.val();             
+            (snapshot) => {
+                let returnedData = snapshot.val();
                 localStorage.setItem(fbTable, JSON.stringify(returnedData));
             },
-            (error) => {              
+            (error) => {
                 localStorage.setItem(fbTable, "error");
                 console.error(error);
                 toastr.error(error.message);
@@ -48,10 +67,10 @@ export class DataService {
         let databaseRef = firebase.database().ref(fbTable);
         databaseRef.once('value',
             (snapshot) => {
-                let returnedData = snapshot.val();               
+                let returnedData = snapshot.val();
                 localStorage.setItem(fbTable, JSON.stringify(returnedData));
             },
-            (error) => {               
+            (error) => {
                 localStorage.setItem(fbTable, "error");
                 console.error(error);
                 toastr.error(error.message);
@@ -84,14 +103,23 @@ export class DataService {
     }  //updateUserProfile
 
     getAllUnits(): void {
-        localStorage.removeItem("allUnits");
-        firebase.database().ref('/Units').once('value')
-            .then((snapshot) => {
-                localStorage.setItem('allUnits', JSON.stringify(snapshot.val()))
-            })
-            .catch((error) => {
-                toastr.error(error.message);
-            })
+        localStorage.setItem("allUnits", "error");  
+        try {
+            localStorage.removeItem("allUnits");
+
+            firebase.database().ref('/Units').once('value')
+                .then((snapshot) => {                 
+                    localStorage.setItem('allUnits', JSON.stringify(snapshot.val()))
+                    this._allUnitsLoaded.emit(true);
+                })
+                .catch((error) => {
+                    toastr.error(error.message);
+                    this._allUnitsLoaded.emit(true);
+                })
+        }
+        catch (ex) {
+            this._allUnitsLoaded.emit(true);
+        }
     }
 
     hasUnitMaximumNumberOfUsers(unitNumber: string): boolean {
@@ -99,6 +127,10 @@ export class DataService {
         return (allUnits[unitNumber].RegisteredUsers >= AppSettings.MAXIMUM_USERS_PER_UNIT);
     }
 
+    getAllUnitsLoaded(): EventEmitter<any> {
+        return this._allUnitsLoaded;
+
+    }
     getUserProfileUpdated(): EventEmitter<any> {
         return this._userProfileUpdated;
     }
--- app/signup.component.ts
+++ app/signup.component.ts
@@ -4,7 +4,7 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 import {AuthService} from "./shared/auth.service";
 import {DataService} from "./shared/data.service";
 import {User} from "./shared/user.interface";
-import  {AppHelpers} from "./app.component";
+import {AppHelpers} from "./app.component";
 
 
 @Component({
@@ -14,20 +14,18 @@ import  {AppHelpers} from "./app.component";
 })
 export class SignupComponent implements OnInit {
     signUpForm: ControlGroup;
+    supportsLocalStorage: boolean = false;
+    isLoading: boolean = true;
+    isError: boolean = false;
 
     constructor(private _fb: FormBuilder, private _authService: AuthService, private _dataService: DataService) {
 
+
+
     }
-    onSignup() {        
-        var user: User = this.signUpForm.value;   
-        if (this._dataService.hasUnitMaximumNumberOfUsers(user.unit)) {
-            toastr.error("The maximum number of users have already signed up for this Unit #.")
-            return;
-        }
-        this._authService.signupUser(this.signUpForm.value);
-    }
+
     ngOnInit(): any {
-        this._dataService.getAllUnits();
+
         this.signUpForm = this._fb.group({
             email: ['', Validators.compose([
                 Validators.required,
@@ -45,7 +43,54 @@ export class SignupComponent implements OnInit {
                 this.isValidUnit
             ])]
         });
+
+        this._dataService.getAllUnitsLoaded().subscribe(() => {         
+            this.isLoading = false;
+            this.supportsLocalStorage = this._dataService.supportsLocalStorage;
+        });
+
+
+        if (!this.isDataReady()) {
+            var retryCount: number = 0;           
+            this._dataService.getAllUnits();
+            //now watch the last firebase call to get when the data is ready.
+            var refreshId = setInterval(() => {
+                retryCount++;               
+                if (this.isDataReady() || this.isError || !this.isLoading || retryCount > 10) {
+                    clearInterval(refreshId);
+                    this.isLoading = false;
+                }
+            }, 500);  //check for data every 1/2 second
+        }
+        else {
+            this.supportsLocalStorage = this._dataService.supportsLocalStorage;
+            this.isLoading = false;
+        }
+
+
     }
+
+    isDataReady(): boolean {
+        let cachedData: any = localStorage.getItem("allUnits");
+        if (cachedData) {
+            if (cachedData === "error") {
+                this.isError = true;
+                return;
+            }
+            return true;
+        }
+        return false;
+    }  //isDataReady
+
+    onSignup() {
+        var user: User = this.signUpForm.value;
+        if (this._dataService.hasUnitMaximumNumberOfUsers(user.unit)) {
+            toastr.error("The maximum number of users have already signed up for this Unit #.")
+            return;
+        }
+        this._authService.signupUser(this.signUpForm.value);
+    }
+
 
     isEmail(control: Control): { [s: string]: boolean } {
         if (!control.value.match(/^[a-z0-9_\+-]+(\.[a-z0-9_\+-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*\.([a-z]{2,4})$/)) {
