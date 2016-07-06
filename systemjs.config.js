0c4cbc11e3f46ed77cfe0b4623935a74592c08d9
Johnny Sims <techiebud@gmail.com>
2016-07-06 10:27:36 -0400

    Code cleanup - removed unnessary modules which are being download from the web (Firebase and BootBox)

--- systemjs.config.js
+++ systemjs.config.js
@@ -8,9 +8,7 @@
     'app': 'js', // 'dist',
     '@angular': 'node_modules/@angular',
     'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-    'rxjs': 'node_modules/rxjs',
-    'firebase': 'node_modules/firebase',
-    'bootbox': 'node_modules/bootbox',
+    'rxjs': 'node_modules/rxjs',   
     'angular2-cookie': 'node_modules/angular2-cookie'
   };
   // packages tells the System loader how to load when no filename and/or no extension
