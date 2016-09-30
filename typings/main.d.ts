cd3985a767c3f4f7f3695f154e81209012f569ce
Techiebud <techiebud@gmail.com>
2016-09-30 12:15:45 -0400

    Upgraded to Typscript 2.0 and removed typings stuff

--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/core-js/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/node/index.d.ts" />
-/// <reference path="globals/jquery/jquery.custom.d.ts" />
